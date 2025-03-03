d24ff6599991f167352ca409d6cbffa322ba2758
ardaoru <160649630+ardaoru@users.noreply.github.com>
2025-03-03 10:32:31 +0300

    for loop created

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ def int_to_roman(num):
     :return: A string representing the Roman numeral of the integer.
     """
     roman_numbers = {1000:"M", 900:"CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L", 40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I" }
+
+    output = []
+    
+    for number, letters in roman_numbers.items
+        while num >= number
+        output.append(letters)
+        num = num - number
+
+    return output              
             
